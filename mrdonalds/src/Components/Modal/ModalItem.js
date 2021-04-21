026814071a65340711df6ef1c90354d5e614bdc7
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-04-21 21:49:54 +0500

    Completed task lesson 9

--- mrdonalds/src/Components/Modal/ModalItem.js
+++ mrdonalds/src/Components/Modal/ModalItem.js
@@ -4,6 +4,8 @@ import { ButtonCheckout } from '../Style/ButtonCheckout';
 import { CountItem } from './CountItem';
 import { useCount } from '../Hooks/useCount';
 import { totalPriceItems, formatCurrency } from '../Functions/secondoryFunction';
+import { Toppings } from './Toppings';
+import { useToppings } from '../Hooks/useToppings';
 
 const Overlay = styled.div`
   position: fixed;
@@ -56,6 +58,7 @@ const TotalPriceItem = styled.div`
 export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
 
   const counter = useCount();
+  const toppings = useToppings(openItem);
   
   const closeModal = (e) => {
     if (e.target.id === 'overlay') {
@@ -65,7 +68,8 @@ export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
 
   const order = {
     ...openItem,
-    count: counter.count
+    count: counter.count,
+    topping: toppings.toppings
   };
 
   const addToOrder = () => {
@@ -83,6 +87,7 @@ export const ModalItem = ({ openItem, setOpenItem, orders, setOrders }) => {
             <div>{formatCurrency(openItem.price)}</div>
           </HeaderContent>
           <CountItem {...counter}/>
+          {openItem.toppings && <Toppings {...toppings}/>}
           <TotalPriceItem>
             <span>Цена:</span>
             <span>{formatCurrency(totalPriceItems(order))}</span>
--- mrdonalds/src/Components/Order/OrderListItem.js
+++ mrdonalds/src/Components/Order/OrderListItem.js
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 import trashImg from '../../image/trash.svg';
 import { totalPriceItems, formatCurrency } from '../Functions/secondoryFunction';
 
+
 const OrderItemStyled = styled.li`
   display: flex;
   margin: 15px 0;
+  flex-wrap: wrap;
 `;
 
 const ItemName = styled.span`
@@ -31,11 +33,23 @@ const TrashButton = styled.button`
   corsor: pointer;
 `;
 
-export const OrderListItem = ({ order }) => (
+const Toppings = styled.div`
+  color: #9a9a9a;
+  font-size: 14px;
+  width: 100%;
+`;
+
+export const OrderListItem = ({ order }) => {
+  const topping = order.topping.filter(item => item.checked)
+    .map(item => item.name)
+    .join(', ');
+
+  return (
   <OrderItemStyled>
     <ItemName>{order.name}</ItemName>
     <span>{order.count}</span>
     <ItemPrice>{formatCurrency(totalPriceItems(order))}</ItemPrice>
     <TrashButton/>
+    {topping && <Toppings>Допы: {topping}</Toppings>}
   </OrderItemStyled>
-);
+)};
