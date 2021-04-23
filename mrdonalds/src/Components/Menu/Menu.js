0fc928fa8c47ac28c00f0f15f67732de27f60e51
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-04-23 11:35:07 +0500

    Completed must task lesson 14

--- mrdonalds/src/Components/Menu/Menu.js
+++ mrdonalds/src/Components/Menu/Menu.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
+import { useFetch } from '../Hooks/useFetch';
+import preloaderImg from '../../image/preloader.svg';
 
 const MenuStyled = styled.main`
   background-color: #ccc;
@@ -14,22 +15,42 @@ const SectionMenu = styled.section`
   padding: 30px;
 `;
 
-export const Menu = ({ setOpenItem }) =>(
-  <MenuStyled>
-    <Banner/>
-    <SectionMenu>
-      <h2>Бургеры</h2>
-      <ListItem 
-        itemList={dbMenu.burger}
-        setOpenItem={setOpenItem}
-      />
-    </SectionMenu>
-    <SectionMenu>
-      <h2>Закуски / Напитки</h2>
-      <ListItem 
-        itemList={dbMenu.other}
-        setOpenItem={setOpenItem}
-      />
-    </SectionMenu>
-  </MenuStyled>
-);
+const Preloader = styled.img`
+  display: flex;
+  width: 35%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Menu = ({ setOpenItem }) => {
+
+  const res = useFetch();
+
+  const dbMenu = res.response;
+
+  return (
+    <MenuStyled>
+      <Banner/>
+      {res.response ?
+      <>
+        <SectionMenu>
+          <h2>Бургеры</h2>
+          <ListItem 
+            itemList={dbMenu.burger}
+            setOpenItem={setOpenItem}
+          />
+        </SectionMenu>
+        <SectionMenu>
+          <h2>Закуски / Напитки</h2>
+          <ListItem 
+            itemList={dbMenu.other}
+            setOpenItem={setOpenItem}
+          />
+        </SectionMenu>
+      </> : res.error ?
+      <Preloader src={preloaderImg} alt='прелоадер'/> :
+      <div>Loading...</div>
+      }
+    </MenuStyled>
+  )
+};
